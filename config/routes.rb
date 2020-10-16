8d68f9adf36529ed535a3b8cc0d31bc5b9aa645d
EC2 Default User <ec2-user@ip-172-31-5-30.ec2.internal>
2020-10-16 06:09:21 +0000

    [Update] Top画面とDevise関連の設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  
+  #管理者deviseのコントローラーのパスを任意のパスに変更
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions'
+  }
+  #会員deviseのコントローラーのパスを任意のパスに変更
+  devise_for :customers,controllers: {
+    registrations: 'customer/registrations',
+    sessions: 'customer/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #ルートパスを会員のTOPページに設定
+  root 'customer/homes#top'
+  #about ページのルーティング
+  get '/about' => 'customer/homes#about'
+  #管理者トップページのルーティング
+  get '/admin' => 'admin/homes#top'
+  
+  
 end
