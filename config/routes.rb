f6bdba6f6a0caed6fb9ab80689a4484e6bd87a6f
masa-8432 <masachi323@gmail.com>
2020-10-17 05:41:10 +0000

    first-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  
+
   # ルートパスを会員TOPページに設定
   root 'customer/homes#top'
   # 会員TOPページ
   get '/admin' => 'admin/homes#top'
   # 会員紹介ページ
   get '/about' => 'customer/homes#about', as: 'about'
-  
-  
+
+
   # devise関連のコントローラのルーティングを任意のパスに変更
   # 会員新規登録画面、ログイン画面のルーティング
   devise_for :customers, controllers: {
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   devise_for :admins, controllers: {
     sessions: 'admin/sessions'
   }
-  
+
   # 管理者側の各機能のルーティング
   # namespaseでURLとコントローラのパスの頭に「/admin」が入るようにする
   namespace :admin do
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :ordered_products, only: [:update]
   end
-  
+
   # 会員側のルーティング
   # scope moduleでコントローラのパスのみ頭に「/customer」が入るようにする
   scope module: :customer do
@@ -48,7 +48,7 @@ Rails.application.routes.draw do
         get 'thanks'
       end
     end
-    resources :products, only: [:index, :show] 
+    resources :products, only: [:index, :show]
     resources :cart_items, only: [:index, :create, :update, :destroy] do
       # RESTfulなURL以外は個別にcollectionで設定する
       collection do
@@ -59,5 +59,5 @@ Rails.application.routes.draw do
     resources :products, only: [:index, :edit, :create, :update, :destroy]
     resources :order_products, only: [:update]
   end
-  
+
 end
