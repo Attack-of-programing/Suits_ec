a3818b21fe2add7df3dd59936fe722ac83672aca
EC2 Default User <ec2-user@ip-172-31-66-161.ec2.internal>
2020-10-19 09:48:16 +0000

    [Update]商品登録機能の実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,8 @@ class Product < ApplicationRecord
   
   # 多重度の設定
   belongs_to :genre
-	has_many :cart_items
-	has_many :order_products
+  has_many :cart_items
+  has_many :order_products
     
   # 画像を設定できるようにする
   attachment :image, destroy: false
@@ -11,7 +11,7 @@ class Product < ApplicationRecord
   # バリデーションチェック
   # 空白時にエラー
   validates :name, :explanation, :genre_id, :after_tax_price, :is_sale, presence: true
-	# 数値以外が入力された場合はエラー
-	validates :after_tax_price, numericality: { only_integer: true }
+  # 数値以外が入力された場合はエラー
+  validates :after_tax_price, numericality: { only_integer: true }
     
 end
