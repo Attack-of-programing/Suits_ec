264ab63d6956170ae69663089530826fc6490f25
EC2 Default User <ec2-user@ip-172-31-5-30.ec2.internal>
2020-10-19 11:25:17 +0000

    [Update]管理者ログイン作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  
+  belongs_to :cart_items
+  belongs_to :orders
+  has_many :genres, dependent: :destroy
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#管理者ログインデータの作成
+Admin.create!(
+  email: "Suits_ec_admin@gmail.com",
+  password: "Suits_ec",
+  password_confirmation: "Suits_ec",
+  )
+  
+  
+  
