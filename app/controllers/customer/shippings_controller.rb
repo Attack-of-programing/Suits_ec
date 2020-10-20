f43890674e2b1684f051a6fa4762ec67d0fa44bb
EC2 Default User <ec2-user@ip-172-31-5-30.ec2.internal>
2020-10-20 05:09:40 +0000

    [Add]デザイン共有

--- app/controllers/customer/shippings_controller.rb
+++ app/controllers/customer/shippings_controller.rb
@@ -1,2 +1,46 @@
 class Customer::ShippingsController < ApplicationController
+
+
+  def index
+    @shippings = Shipping.all
+    @shipping = Shipping.new
+
+  end
+
+  def create
+    @shipping = Shipping.new(shipping_params)
+    if @shipping.save
+      redirect_to shippings_path, notice: "You have created shipping data successfully."
+    else
+      @shippings = Shipping.all
+      render "index"
+    end
+  end
+
+  def destroy
+     @shipping =Shipping.find(params[:id])
+     @shipping.destroy
+     redirect_to shippings_path
+  end
+
+  def edit
+    @shipping =Shipping.find(params[:id])
+  end
+
+  def update
+    @shipping =Shipping.find(params[:id])
+    if @shipping.update(shipping_params)
+      redirect_to shippings_path,notice: "You have updated shipping data successfully."
+    else
+      render "edit"
+    end
+  end
+
+private
+
+  def shipping_params
+    params.require(:shipping).permit(:postcode,:address,:name)
+  end
+
+
 end
--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,2 +1,8 @@
 class Shipping < ApplicationRecord
+ 
+  belongs_to :customers
+ 
+  validates :postcode, presence: true
+	validates :address, presence: true
+  validates :name, presence: true
 end
