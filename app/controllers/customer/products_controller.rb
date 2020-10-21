43fa32943a21e4838f41d326993226073567bfa7
EC2 Default User <ec2-user@ip-172-31-66-161.ec2.internal>
2020-10-21 07:37:02 +0000

    [Update]会員側の商品一覧画面の作成。

--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -1,2 +1,24 @@
 class Customer::ProductsController < ApplicationController
+    
+  def index
+    # 有効ジャンルを全て取得
+    @genres = Genre.where(is_active: true).all
+    if params[:genre_id]
+      # ジャンル検索の場合、ジャンルが有効であるかつ検索ジャンルに当てはまる商品を取得
+      @products = Product.eager_load(:genre).where(genres: {is_active: true}, genre_id: params[:genre_id]).page(params[:page]).per(8)
+      # 検索ジャンルの取得
+      @genre = Genre.find(params[:genre_id])
+    else
+      # 商品全表示の場合、ジャンルが有効である商品を全て取得する
+      @products = Product.eager_load(:genre).where(genres: {is_active: true}).all.page(params[:page]).per(8)
+    end
+  end
+  
+  private
+  # ストロングパラメータ
+  def product_params
+    params.require(:product).permit(:name, :image, :explanation,
+       :genre_id, :after_tax_price, :is_sale)
+  end  
+  
 end
