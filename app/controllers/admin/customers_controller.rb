20ae5d4551cea0bb3da999b90a1124afc4491364
masa-8432 <masachi323@gmail.com>
2020-10-22 06:45:16 +0000

    [Update]管理者側の会員一覧・編集画面作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,33 @@
 class Admin::CustomersController < ApplicationController
+
+  def index
+    @customers = Customer.page(params[:page]).per(10)
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+      flash[:notice] = "会員情報を変更しました"
+    else
+      @customers = Customer.all
+      render 'edit'
+    end
+
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email, :is_deleted )
+  end
+
 end
