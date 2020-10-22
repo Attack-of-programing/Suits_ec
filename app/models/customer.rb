28d8d59b2d676f64ab1d10db4d27d2aebbc6bc1e
EC2 Default User <ec2-user@ip-172-31-66-161.ec2.internal>
2020-10-22 08:54:45 +0000

    [Update]カート機能の実装。

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,5 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # 多重度の設定
   has_many :shippings,dependent: :destroy
+  has_many :cart_items,dependent: :destroy
+  
 end
