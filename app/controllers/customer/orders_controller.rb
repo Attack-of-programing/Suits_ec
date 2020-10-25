ca6d3212a69602f01062ab36e6d2ae1688fea147
Shingo-Kuroyanagi <ec2-user@ip-172-31-5-30.ec2.internal>
2020-10-25 06:31:15 +0000

    [Update]注文cotroller作成

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,2 +1,15 @@
 class Customer::OrdersController < ApplicationController
+
+    def index       
+        @orders = Order.all
+        @orders = current_customer.orders
+    end
+
+    def show
+        @order = Order.find(params[:id])
+    end
+
+
+
+
 end
