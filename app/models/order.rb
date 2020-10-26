9664a78055b3d0b47cffdbbf38ac21d28b593b94
masa-8432 <masachi323@gmail.com>
2020-10-26 08:30:37 +0000

    [Upload]ビューの修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,22 +1,22 @@
 class Order < ApplicationRecord
 
-    #カラムとして存在しないものをメソッドとして定義する
-    attr_accessor :shipping_id
+  belongs_to :customer, optional: true
+  has_many :order_products, dependent: :destroy
 
-    # バリデーション
-    validates :postcode, presence: true, length: { is: 7 }, format: { with: /\A[0-9]+\z/ }
-    validates :address, presence: true
-    validates :name, presence: true  
-  
-    # 他モデルにもデータを登録したい場合
-    accepts_nested_attributes_for :order_products
-    
-    # 多重度の設定
-    belongs_to :customer
-    has_many :order_products, dependent: :destroy
-    
-    # 列挙型変数の定義
-    enum pay_method: {"クレジットカード": 0,"銀行振込": 1}
-	  enum order_status: {"入金待ち": 0,"入金確認": 1,"製作中": 2,"発送準備中": 3, "発送済み": 4}
-    
+  #カラムとして存在しないものをメソッドとして定義する
+  attr_accessor :shipping_id
+
+  # バリデーション
+  validates :postcode, presence: true, length: { is: 7 }, format: { with: /\A[0-9]+\z/ }
+  validates :address, presence: true
+  validates :name, presence: true
+
+  # 画像表示
+  attachment :image
+
+  # ENUMの設定
+  enum pay_method: { "クレジットカード": 0, "銀行振込": 1 }
+  enum order_status: {"入金待ち":0, "入金確認":1, "製作中":2, "発送準備中":3, "発送済み":4 }
+
+
 end
